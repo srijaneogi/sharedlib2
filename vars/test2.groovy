88b4df0a91bbcd70f9a429713ba13d5bd7ed6356
srijaneogi <65847605+srijaneogi@users.noreply.github.com>
2022-02-28 10:57:15 +0530

    Create test2.groovy

---
+++ vars/test2.groovy
@@ -0,0 +1,12 @@
+node {
+    def mvnHome = "Hello from shared library 2"
+    stage('Preparation') { // for display purposes
+        echo "$mvnHome"
+    }
+    stage('Build') {
+        
+    }
+    stage('Results') {
+        
+    }
+}
